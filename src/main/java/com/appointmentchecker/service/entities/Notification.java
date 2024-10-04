a8a7734166e693aed7923b24fb3122dd0bf3ec00
laura <103428878+Lau-r-a@users.noreply.github.com>
2024-10-04 15:41:39 +0200

    prevent user object in response

--- src/main/java/com/appointmentchecker/service/entities/Notification.java
+++ src/main/java/com/appointmentchecker/service/entities/Notification.java
@@ -1,5 +1,6 @@
 package com.appointmentchecker.service.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 @Getter
 @Setter
 @AllArgsConstructor
+@JsonIgnoreProperties(value = { "user" })
 public class Notification {
 
     @Id
